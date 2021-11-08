2a36876739dd0ad85d60f9be9cfc25b16a1fe5e8
Juan Pleitez <juanpc13lolol@gmail.com>
2021-11-08 02:07:49 -0600

    Agregando Menu de DropDown o Select para Pais

---
+++ src/main/java/ues/edu/sv/administrarclientes/appvista/AdministrarClientesView.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ues.edu.sv.administrarclientes.appvista;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Juan
+ */
+@Named
+@RequestScoped
+public class AdministrarClientesView {
+    
+}
