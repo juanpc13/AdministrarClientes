41c783dc1f82862586da2dc74471daeaec7620ba
Juan Pleitez <juanpc13lolol@gmail.com>
2021-11-08 20:00:43 -0600

    Agregando nuevo hostname al proyecto apra conectarse

--- src/main/java/ues/edu/sv/administrarclientes/appvista/AdministrarClientesView.java
+++ src/main/java/ues/edu/sv/administrarclientes/appvista/AdministrarClientesView.java
@@ -11,9 +11,14 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import ues.edu.sv.administrarclientes.controlador.ClienteFacade;
 import ues.edu.sv.administrarclientes.controlador.PaisFacade;
+import ues.edu.sv.administrarclientes.controlador.TipoFacade;
 import ues.edu.sv.administrarclientes.entidades.Cliente;
+import ues.edu.sv.administrarclientes.entidades.Direccion;
 import ues.edu.sv.administrarclientes.entidades.Pais;
+import ues.edu.sv.administrarclientes.entidades.Tienda;
+import ues.edu.sv.administrarclientes.entidades.Tipo;
 
 /**
  *
@@ -26,10 +31,15 @@ public class AdministrarClientesView {
     // Controladores
     @Inject
     private PaisFacade paisFacade;
+    @Inject
+    private ClienteFacade clienteFacade;
+    @Inject
+    private TipoFacade tipoFacade;
     
     // Listas de los datos
     private List<Pais> paises;
     private List<Cliente> clientes;
+    private List<Tipo> tipos;
     
     // Selecciones de la vista
     private Pais selectedPais;
@@ -41,9 +51,25 @@ public class AdministrarClientesView {
     public void init() {
         // Inicializando como new las variables
         paises = new ArrayList<>();
-        tempCliente = new Cliente();
+        clientes = new ArrayList<>();
+        tipos = new ArrayList<>();
+        clearTempClient();
         
+        // Cargar las listas
         paises = paisFacade.findAll();
+        clientes = clienteFacade.findAll();
+        tipos = tipoFacade.findAll();
+        
+        for (Cliente cliente : clientes) {
+            System.out.println(cliente.getNombres());
+        }
+    }
+    
+    public void clearTempClient(){
+        tempCliente = new Cliente();
+        tempCliente.setTiendaId(new Tienda());
+        tempCliente.setDireccionId(new Direccion());
+        //tempCliente.setTipoList(new ArrayList<Tipo>()); //NO SE CREAR AQUI
     }
 
     public List<Pais> getPaises() {
@@ -62,9 +88,18 @@ public class AdministrarClientesView {
         this.clientes = clientes;
     }
 
+    public List<Tipo> getTipos() {
+        return tipos;
+    }
+
+    public void setTipos(List<Tipo> tipos) {
+        this.tipos = tipos;
+    }
+
     public Pais getSelectedPais() {
         return selectedPais;
     }
+    
 
     public void setSelectedPais(Pais selectedPais) {
         this.selectedPais = selectedPais;
@@ -79,5 +114,12 @@ public class AdministrarClientesView {
     }
     
     
+    public void onRowSelect(){
+        System.out.println(tempCliente.getNombres());
+    }
+    
+    public void onItemSelect(){
+        System.out.println(tempCliente.getTipoList());
+    }
     
 }
